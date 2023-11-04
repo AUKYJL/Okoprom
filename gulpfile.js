1acea6685775c0767d605f1c58ec818700d762f9
AUKYJL <89800289+AUKYJL@users.noreply.github.com>
2023-11-04 12:02:44 +0500

    created deployToGHPages

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ import { path } from "./config/gulp-settings.js";
 
 // Передаємо значення у глобальну змінну
 global.app = {
-	isBuild: process.argv.includes('--build'),
-	isDev: !process.argv.includes('--build'),
-	isWebP: !process.argv.includes('--nowebp'),
-	isFontsReW: process.argv.includes('--rewrite'),
-	gulp: gulp,
-	path: path,
-	plugins: plugins
-}
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
+  isWebP: !process.argv.includes("--nowebp"),
+  isFontsReW: process.argv.includes("--rewrite"),
+  gulp: gulp,
+  path: path,
+  plugins: plugins,
+};
 
 // Імпорт завдань
 import { reset } from "./config/gulp-tasks/reset.js";
@@ -28,24 +28,31 @@ import { zip } from "./config/gulp-tasks/zip.js";
 import { sprite } from "./config/gulp-tasks/sprite.js";
 import { gitignore } from "./config/gulp-tasks/gitignore.js";
 import { otfToTtf, ttfToWoff, fonstStyle } from "./config/gulp-tasks/fonts.js";
+import { deploy } from "./config/gulp-tasks/deployToGHPages.js";
 
 // Послідовна обробка шрифтів
 const fonts = gulp.series(reset, otfToTtf, ttfToWoff, fonstStyle);
 // Основні завдання виконуватимемо паралельно після обробки шрифтів
 const devTasks = gulp.parallel(fonts, gitignore);
 // Основні завдання виконуватимемо паралельно після обробки шрифтів
-const buildTasks = gulp.series(fonts, jsDev, js, gulp.parallel(html, css, images, gitignore));
+const buildTasks = gulp.series(
+  fonts,
+  jsDev,
+  js,
+  gulp.parallel(html, css, images, gitignore),
+  deploy
+);
 
 // Експорт завдань
-export { html }
-export { css }
-export { js }
-export { jsDev }
-export { images }
-export { fonts }
-export { sprite }
-export { ftp }
-export { zip }
+export { html };
+export { css };
+export { js };
+export { jsDev };
+export { images };
+export { fonts };
+export { sprite };
+export { ftp };
+export { zip };
 
 // Побудова сценаріїв виконання завдань
 const development = gulp.series(devTasks);
@@ -54,16 +61,10 @@ const deployFTP = gulp.series(buildTasks, ftp);
 const deployZIP = gulp.series(buildTasks, zip);
 
 // Експорт сценаріїв
-export { development }
-export { build }
-export { deployFTP }
-export { deployZIP }
+export { development };
+export { build };
+export { deployFTP };
+export { deployZIP };
 
 // Виконання сценарію за замовчуванням
-gulp.task('default', development);
-
-
-
-
-
-
+gulp.task("default", development);
