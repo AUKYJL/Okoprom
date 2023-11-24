9eb8d85a6013e28286138562ffe531107d4f2cf2
AUKYJL <89800289+AUKYJL@users.noreply.github.com>
2023-11-24 23:52:58 +0500

    updated dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 // Імпорт основного модуля
 import gulp from "gulp";
 // Імпорт загальних плагінів
-import { plugins } from "./config/gulp-plugins.js";
+import {plugins} from "./config/gulp-plugins.js";
 // Імпорт шляхів
-import { path } from "./config/gulp-settings.js";
+import {path} from "./config/gulp-settings.js";
 
 // Передаємо значення у глобальну змінну
 global.app = {
   isBuild: process.argv.includes("--build"),
+  //удаляет первую встречную страницу, оставляя непосредственно только сам дизайн сайта
+  isBuildWOIndex: process.argv.includes("--buildWithoutIndex"),
   isDev: !process.argv.includes("--build"),
   isWebP: !process.argv.includes("--nowebp"),
   isFontsReW: process.argv.includes("--rewrite"),
@@ -17,18 +19,19 @@ global.app = {
 };
 
 // Імпорт завдань
-import { reset } from "./config/gulp-tasks/reset.js";
-import { html } from "./config/gulp-tasks/html.js";
-import { css } from "./config/gulp-tasks/css.js";
-import { js } from "./config/gulp-tasks/js.js";
-import { jsDev } from "./config/gulp-tasks/js-dev.js";
-import { images } from "./config/gulp-tasks/images.js";
-import { ftp } from "./config/gulp-tasks/ftp.js";
-import { zip } from "./config/gulp-tasks/zip.js";
-import { sprite } from "./config/gulp-tasks/sprite.js";
-import { gitignore } from "./config/gulp-tasks/gitignore.js";
-import { otfToTtf, ttfToWoff, fonstStyle } from "./config/gulp-tasks/fonts.js";
-//import { deploy } from "./config/gulp-tasks/deployToGHPages.js";
+import {reset} from "./config/gulp-tasks/reset.js";
+import {html} from "./config/gulp-tasks/html.js";
+import {css} from "./config/gulp-tasks/css.js";
+import {js} from "./config/gulp-tasks/js.js";
+import {jsBuildWOI} from "./config/gulp-tasks/js-buildWOI.js";
+import {jsDev} from "./config/gulp-tasks/js-dev.js";
+import {images} from "./config/gulp-tasks/images.js";
+import {ftp} from "./config/gulp-tasks/ftp.js";
+import {zip} from "./config/gulp-tasks/zip.js";
+import {sprite} from "./config/gulp-tasks/sprite.js";
+import {gitignore} from "./config/gulp-tasks/gitignore.js";
+import {setNewIndexPage} from "./config/gulp-tasks/setNewIndexPage.js";
+import {otfToTtf, ttfToWoff, fonstStyle} from "./config/gulp-tasks/fonts.js";
 
 // Послідовна обробка шрифтів
 const fonts = gulp.series(reset, otfToTtf, ttfToWoff, fonstStyle);
@@ -41,29 +44,38 @@ const buildTasks = gulp.series(
   js,
   gulp.parallel(html, css, images, gitignore)
 );
+const buildWOITasks = gulp.series(
+  fonts,
+  jsDev,
+  jsBuildWOI,
+  gulp.parallel(html, css, images, gitignore),
+  setNewIndexPage
+);
 
 // Експорт завдань
-export { html };
-export { css };
-export { js };
-export { jsDev };
-export { images };
-export { fonts };
-export { sprite };
-export { ftp };
-export { zip };
+export {html};
+export {css};
+export {js};
+export {jsDev};
+export {images};
+export {fonts};
+export {sprite};
+export {ftp};
+export {zip};
 
 // Побудова сценаріїв виконання завдань
 const development = gulp.series(devTasks);
 const build = gulp.series(buildTasks);
+const buildWOI = gulp.series(buildWOITasks);
 const deployFTP = gulp.series(buildTasks, ftp);
 const deployZIP = gulp.series(buildTasks, zip);
 
 // Експорт сценаріїв
-export { development };
-export { build };
-export { deployFTP };
-export { deployZIP };
+export {development};
+export {build};
+export {buildWOI};
+export {deployFTP};
+export {deployZIP};
 
 // Виконання сценарію за замовчуванням
 gulp.task("default", development);
